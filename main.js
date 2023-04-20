8cc8e4f7956c588566de1d9cfd3796b3cde10205
rrdmp15 <rdayismar@gmail.com>
2023-04-19 23:45:08 -0500

    test: :test_tube: se testean diferentes formas de hacer el diseño

--- main.js
+++ main.js
@@ -1,44 +1,17 @@
 const pokemonContainer = document.querySelector(".pokemonContainer")
+let pokemon = [];
+const fetchPokemon = async()=>{
+    try{
+        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0`);
+        
+        const data = await respuesta.json();
+        console.log(data);
 
-function fetchPokemon(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((res) => res.json())
-    .then((data)=> {
-        createPokemon(data);
-    })
-}
-
-function fetchPokemons(number) {
-    for (let i = 1; i <= number; i++) {
-        fetchPokemon(i);
+    }catch(error){
+        console.error(error);
     }
+    
 }
 
-function createPokemon(pokemon){
-    const card = document.createElement('div');
-    card.classList.add('pokemon-block');
-
-    const spriteContainer = document.createElement('div');
-    spriteContainer.classList.add('img-container');
-
-    const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.front_default;
-
-    spriteContainer.appendChild(sprite);
-
-    const number = document.createElement('p')
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-
-    const name = document.createElement('p');
-    name.classList.add('name');
-    name.textContent = pokemon.name;
-
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.appendChild(name);
-
-    pokemonContainer.appendChild(card);
-}
-
-fetchPokemons(1010)
+fetchPokemon();
 //``
