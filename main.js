a39a291dd1e342e54720173a39d2097482ae7e5b
rrdmp15 <rdayismar@gmail.com>
2023-04-20 23:54:42 -0500

    perf: :construction: Se inicio la funcionalidad

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
 import style from "../components/style.js"
+import paginacion from "../components/paginacion.js"
+
 
 style.showPokedex();
 
 
 
-const pokemonContainer = document.querySelector(".pokemonContainer")
-let pokemon = [];
-const fetchPokemon = async()=>{
-    try{
-        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0`);
-        
-        const data = await respuesta.json();
-        console.log(data);
+const lista = document.querySelector(".lista");
+const paginacionContainer = document.querySelector(".paginacionContainer");
+let urlPokemon = "https://pokeapi.co/api/v2/pokemon";
+let btnNext;
+let btnPrevious;
+let plantilla;
 
+const fetchPokemon = async(url)=>{
+    try{
+        const respuesta = await fetch(url);
+        
+        const resultado = await respuesta.json();
+
+        console.log(resultado);
+        dataPokemons(resultado.results)
+
+        btnNext = resultado.next ? `<button class="btn next" data-url=${resultado.next}><i class="fa-solid fa-forward-fast"></i></button>` : "";
+         btnPrevious = resultado.previous ? `<button class="previuos btn" data-url=${resultado.previous}><i class="fa-solid fa-backward-fast"></i></button>` : "";
+
+        paginacionContainer.innerHTML = btnNext + "" +btnPrevious;
     }catch(error){
         console.error(error);
-    }
-    
+    }  
 }
 
-fetchPokemon();
+const dataPokemons = async(data)=>{
+    lista.innerHTML = "";
+    try {
+        for(let i of data){
+            const resp = await fetch(i.url);
+            const resul = await resp.json();
+
+            console.log(resul) 
+
+            plantilla = `
+            <li><img src="img/pokebolaMini.png" alt=""> ${resul.id} ${resul.name}</li>
+            `;
+            
+            lista.innerHTML += plantilla;
+        }
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+paginacionContainer.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("btn")){
+        let value = e.target.dataset.url;
+        console.log(value)
+        fetchPokemon(value)
+    }
+})
+
+fetchPokemon(urlPokemon);
 //``
